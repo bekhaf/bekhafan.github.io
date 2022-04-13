72046264551e4ebcf8e2d605f1d9ac1134def446
Pouriya <pouriya.raj@gmail.com>
2022-04-14 01:00:18 +0430

    Add vocalists

--- global/scripts/links.js
+++ global/scripts/links.js
@@ -3,16 +3,20 @@ const linkGroups = document.querySelectorAll('.link-group');
 for (let i = 0; i < linkGroups.length; i++) {
   const lg = linkGroups[i];
   const btn = lg.querySelector('.link-group__btn');
+  const btnComputedStyle = btn.currentStyle || getComputedStyle(btn);
+  const btnMarginTop = +btnComputedStyle['margin-top'].slice(0, -2)
+  const btnMarginBottom = +btnComputedStyle['margin-bottom'].slice(0, -2)
+  const btnHeight = (btn.clientHeight + btnMarginTop + btnMarginBottom);
   const links = lg.querySelector('.link-group__links');
+  const linksHeight = +getComputedStyle(links)['height'].slice(0, -2);
 
   btn.addEventListener('click', () => {
-    console.log(lg)
     if (lg.classList.contains('link-group--active')) {
       lg.classList.remove('link-group--active');
       lg.style.height = '';
     } else {
       lg.classList.add('link-group--active');
-      lg.style.height = links.children.length * 30 + 54 + 'px';
+      lg.style.height = linksHeight + btnHeight + 'px';
     }
   })
 }
